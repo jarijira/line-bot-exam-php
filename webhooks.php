35f72261735bfac16c03d7ac9299b67cffdf3661
Jira Boonudom <jarijira@gmail.com>
2018-12-28 09:05:54 +0700

    Update Line Access Token

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'teIE43BSumN6Fg+MMa97MXpAR60vGI3n6dTx4Ipbp2y9mZH+E0FxlA01MBcZR7g4iFTGrrSJO77wXFEV1llajEaFABKfwJ/N+ZjImhGLNuQ8s4eBShXtlzGWiI1OKe5QwUC6/7z6sDsYOZ9CVeQbDAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
